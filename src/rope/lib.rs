ad128337f537b1b657296ff3a07f4c50f3f15812
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-03 15:08:57 -0700

    Add basic types

--- src/rope/lib.rs
+++ src/rope/lib.rs
@@ -1,3 +1,32 @@
-#[test]
-fn it_works() {
-}
+#![crate_name="rope"]
+#![crate_type="lib"]
+
+#![license = "MIT"]
+#![doc(html_root_url = "http://darinmorrison.github.io/rope.rs/doc/rope/")]
+
+#![feature(struct_variant)]
+
+//! This crate implements the Rope data type.
+
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+extern crate algebra;
+extern crate fingertree;
+
+use fingertree::{
+    Deep,
+    Digit,
+    Empty,
+    FingerTree,
+    Four,
+    One,
+    Single,
+    Three,
+    Two,
+};
+
+struct Offset(i32);
+struct Chunk(Vec<u8>);
+struct Body(FingerTree<Offset,Chunk>);
+struct Rope(Body);
