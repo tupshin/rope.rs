6201b713f4272e3a7cac29f4d5c93a14123169a4
Darin Morrison <darinmorrison+git@gmail.com>
2014-11-18 15:26:46 -0700

    Remove struct_variant feature (defaulted now)

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@
 #![license = "MIT"]
 #![doc(html_root_url = "http://www.rust-ci.org/epsilonz/rope.rs/doc/rope/")]
 
-#![feature(struct_variant)]
-
 //! This crate implements the Rope data type.
 
 extern crate algebra;
